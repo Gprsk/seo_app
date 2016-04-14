fa8e59b759dcdef0aeca232d8c4fc86624815570
Sk <gprsk95@gmail.com>
2016-04-14 23:56:29 +0000

    First title tests

--- app/controllers/seo_main_controller.rb
+++ app/controllers/seo_main_controller.rb
@@ -1,3 +1,5 @@
+#http://ruby-doc.org/core-2.1.2/String.html#method-i-length
+
 class SeoMainController < ApplicationController
   def index
   end
@@ -7,18 +9,34 @@ class SeoMainController < ApplicationController
 
   	require 'open-uri'
 	  @doc = Nokogiri::HTML(open(@url))
-	  
 	  @titles = @doc.xpath("//title")
 	  @metas = @doc.xpath("//meta")
 	  
+	  #Array with results of the tests
+	  @results = []
+	  
+	  unless @doc.to_s.valid_encoding?
+	  	@doc.to_s.force_encoding("utf-8")
+	  end
+
+	  result = Struct.new(:tag, :content, :score)
 	  #Loop - title check
 	  @titles.each do |title|
+	  	text = title.text.to_s
 	    #Remove namespaces and tags?
 	    #title.replace(title.text) #->not working
 	    #title.replace(Nokogiri::XML::Text.new(title.inner_html, title.document)) # ->not working
-	    puts title.to_s
+	    
+	    #Length
+	    if text.length < 56
+	    	@results << result.new("title", "cefsa", "0")
+	    else
+	    	@results << result.new("title", "cefsa", "10")
+	    end
+	    puts @results.map{|result| result.score}
 	  end
 	  
+	 
 	  #Loop - metatags check
 	  @metas.each do |meta|
 	    #puts meta
